e4d2eddfa5e6baa5810837ecd73bef666a5b2eb4
Tina Conway <tinaconway@Tinas-MacBook-Air.local>
2015-10-07 19:23:13 -0400

    contact form

--- contact.php
+++ contact.php
@@ -5,7 +5,6 @@
 
 
 header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
 $admin_email = 'kc54861@gmail.com'; // Your Email
